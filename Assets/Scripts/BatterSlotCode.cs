b70baa596be005b6e051a428be3f00858e5fe4fa
JamesonFoster <jamesonbfoster@gmail.com>
2025-09-27 02:25:49 -0500

    Added Firing Weapons And Door  STuuff

---
+++ Assets/Scripts/BatterSlotCode.cs
@@ -0,0 +1,33 @@
+using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class BatterSlotCode : MonoBehaviour
+{
+    public bool isActive = false;
+    public GameObject visual;
+    public GameObject doorLink;
+    public Vector2 pos = new Vector2(0,0);
+    private Vector2 qaid;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        qaid = doorLink.transform.position;
+    }
+    void Update()
+    {
+        if (isActive)
+        {
+            visual.SetActive(true);
+            doorLink.transform.position = Vector3.MoveTowards(doorLink.transform.position, qaid + pos, 2 * Time.deltaTime);
+        }    
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Battery"))
+        {
+            isActive = true;
+        }
+    }
+}
