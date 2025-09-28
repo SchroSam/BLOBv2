d4b7dd7a42b0a2c7086ee3eb987df14e0646f7ce
JamesonFoster <jamesonbfoster@gmail.com>
2025-09-27 20:46:22 -0500

    Added Arms Stuff

--- Assets/Scripts/SpawnOnPlayer.cs
+++ Assets/Scripts/SpawnOnPlayer.cs
@@ -6,6 +6,7 @@ public class SpawnOnPlayer : MonoBehaviour
 {
     [Header("Assign in Inspector")]
     public GameObject spritePrefab;   // The sprite to spawn
+    public GameObject spritePrefabLeg;
     public Transform player;          // Reference to the player
     public float maxDist = 10;
     private float rval1;
@@ -39,11 +40,41 @@ public class SpawnOnPlayer : MonoBehaviour
             arms.Add(spawned);
         }
     }
+    public void SpawnSpriteLeg()
+    {
+        rval1 = Random.Range(0, maxDist); // The distance from center
+        rval2 = Random.Range(0, 359); // Rotation of the object
+        rval3 = Random.Range(0, 359) * Mathf.Deg2Rad; // Rotation relative of the center
+
+        float offsetX = Mathf.Cos(rval3) * rval1;
+        float offsety = Mathf.Sin(rval3) * rval1;
+
+        if (spritePrefab != null && player != null)
+        {
+            // Spawn at player's position, no rotation
+            Vector3 spawnPosition = transform.position + new Vector3(offsetX, offsety, 0);
+
+            // Instantiate the object at the calculated position
+            GameObject spawned = Instantiate(spritePrefabLeg, spawnPosition, Quaternion.Euler(0, 0, rval2));
+
+            // Make the sprite a child of the player so it sticks
+            spawned.transform.SetParent(player);
+
+            legs.Add(spawned);
+        }
+    }
     public void KillLimbArm()
     {
         ranob =  Random.Range(0, arms.Count);
         myObject = arms[ranob];
         arms.Remove(arms[ranob]);
+        Destroy(myObject);
+    }
+    public void KillLimbLeg()
+    {
+        ranob = Random.Range(0, legs.Count);
+        myObject = legs[ranob];
+        legs.Remove(legs[ranob]);
         Destroy(myObject);
     }
 }
