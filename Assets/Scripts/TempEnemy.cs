45b12f1070c6f53ec3e1c80fd2f21b07ed77b537
JamesonFoster <jamesonbfoster@gmail.com>
2025-09-27 21:14:06 -0500

    Fixed Leg Shot

--- Assets/Scripts/TempEnemy.cs
+++ Assets/Scripts/TempEnemy.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class TempEnemy : MonoBehaviour
 {
     public GameObject player;
+    public int type;
     // Trigger collider detects the player
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,10 +17,21 @@ public class TempEnemy : MonoBehaviour
 
     void SpawnSpriteOnPlayer()
     {
-        var spawner = FindFirstObjectByType<SpawnOnPlayer>();
-        if (spawner != null)
+        if (type == 0)
         {
-            spawner.SpawnSprite();
+            var spawner = FindFirstObjectByType<SpawnOnPlayer>();
+            if (spawner != null)
+            {
+                spawner.SpawnSprite();
+            }
+        }
+        if (type == 1)
+        {
+            var spawner = FindFirstObjectByType<SpawnOnPlayer>();
+            if (spawner != null)
+            {
+                spawner.SpawnSpriteLeg();
+            }
         }
     }
 }
