508ff722b55d05863945ccbfe4e798fbc97c9e22
JamesonFoster <jamesonbfoster@gmail.com>
2025-09-28 00:38:13 -0500

    Deadly UI

--- Assets/Scripts/UIButtons.cs
+++ Assets/Scripts/UIButtons.cs
@@ -7,8 +7,10 @@ public class UIButtons : MonoBehaviour
     public GameObject MainMenu;
     public GameObject PlayMenu;
     // Start is called before the first frame update
-    void Start()
+    public void TitileNowButton()
     {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("BlobPhysTest");
     }
 
     public void PlayNowButton()
