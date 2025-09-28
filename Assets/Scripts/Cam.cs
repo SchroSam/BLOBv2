db4ee339cafea6cb4e0870c842da17e9463f922c
Schrod <schrosam@gmail.com>
2025-09-27 23:20:26 -0500

    Revert "New Camera Script"
    This reverts commit 524b6bea79d512c7ac9239404e503487f1b500e0.

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -1,34 +1,28 @@
-using System;
 using UnityEngine;
 
 public class Cam : MonoBehaviour
 {
     public GameObject target;
+    private float yspot;
     public int type;
-    public float smoothTime = 0.3f;    // Damping time
-    private Vector3 velocity = Vector3.zero;
-    Vector3 targetTransform;
 
     // Update is called once per frame
     private void Start()
     {
-
-
+        if (type == 0)
+        {
+            yspot = transform.position.y;
+        }
     }
     void Update()
     {
-        //if (Math.Abs(target.GetComponent<Rigidbody2D>().linearVelocity.y) > minYspeed)
-        //{
-        targetTransform = target.transform.position;
-        targetTransform.z = transform.position.z;
-
-        transform.position = Vector3.SmoothDamp(transform.position, targetTransform, ref velocity, smoothTime);
-
-
+        if (type == 0)
+        {
+            transform.position = new Vector3(target.transform.position.x, yspot, -10);
+        }
+        else
+        {
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10);
+        }
     }
-        // else
-        // {
-        //     transform.position = new Vector3(target.transform.position.x, yspot, -10);
-        // }
-        // }
 }
