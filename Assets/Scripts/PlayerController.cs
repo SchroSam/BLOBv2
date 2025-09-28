018b561d2d65dd706645cf1dc1b8e672a3f12b26
JamesonFoster <jamesonbfoster@gmail.com>
2025-09-27 23:46:55 -0500

    Added Battery Again

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -105,6 +105,7 @@ public class Experiment : MonoBehaviour
                 batCount -= 1;
                 InventoryManager.Instance.UpdateUIFromPlayer(this);
                 GameObject newObject = Instantiate(batShot, transform.position, Quaternion.identity);
+                gameObject.GetComponent<SpawnOnPlayer>().KillLimbBat();
 
 
                 if (dire > 0)
