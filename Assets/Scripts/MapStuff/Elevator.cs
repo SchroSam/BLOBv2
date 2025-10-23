f41af05fed73e743d9cdd001825c64cc3b4d2516
Schrod <schrosam@gmail.com>
2025-10-22 22:26:23 -0500

    All is right in the world
    I removed the last traces of Jameson's stupid edits to my Elevator script.

--- Assets/Scripts/MapStuff/Elevator.cs
+++ Assets/Scripts/MapStuff/Elevator.cs
@@ -1,118 +1,183 @@
-using Unity.VisualScripting;
+// using Unity.VisualScripting;
+// using UnityEngine;
+// using System.Collections;
+// using NUnit.Framework;
+// using UnityEngine.UIElements;
+
+// public class Elevator : MonoBehaviour
+// {
+//     public Transform targetTransform;
+//     private Vector3 originalPosition;
+//     public bool playerTrigger = false;
+//     public bool returning = false;
+//     public bool returnStarted = false;
+//     public float speed = 0.3f;
+//     private float velocity;
+//     public float playerSenseDelay = 2.0f;
+//     public float returnDelay = 3.0f;
+
+//     public void Start()
+//     {
+//         originalPosition = transform.position;
+//     }
+
+//     public void OnTriggerEnter2D(Collider2D collision)
+//     {
+       
+//             if (collision.gameObject.tag == "Player" && !playerTrigger && !returning)
+//             {
+//                 StartCoroutine(PauseThenContinue(playerSenseDelay));
+//             }
+//     }
+
+//     public void FixedUpdate()
+//     {
+//         if (playerTrigger)
+//         {
+
+            
+//             if (targetTransform.position != transform.position)
+//             {
+
+//                 if (targetTransform.position.y < transform.position.y)
+//                     velocity = -speed;
+//                 else
+//                     velocity = speed;
+
+//                 transform.position = new Vector3(transform.position.x, transform.position.y + velocity, transform.position.z);
+
+//                 if ((transform.position.y <= targetTransform.position.y && velocity < 0) || (transform.position.y >= targetTransform.position.y && velocity > 0))
+//                 {
+
+//                     playerTrigger = false;
+//                     returning = true;
+//                     Debug.Log("returning");
+                    
+//                     Debug.Log("arrived at target: " + targetTransform.position.y);
+//                 }
+//             }
+
+//         }
+
+//         else if (returning && returnStarted)
+//         {
+            
+//             if (originalPosition != transform.position)
+//             {
+
+//                 if (originalPosition.y < transform.position.y)
+//                     velocity = -speed;
+//                 else
+//                     velocity = speed;
+
+//                 transform.position = new Vector3(transform.position.x, transform.position.y + velocity, transform.position.z);
+
+//                 if ((transform.position.y <= originalPosition.y && velocity < 0) || (transform.position.y >= originalPosition.y && velocity > 0))
+//                 {
+                    
+//                     returning = false;
+//                     returnStarted = false;
+                    
+//                     Debug.Log("arrived back home: " + originalPosition.y);
+//                 }
+//             }
+//         }
+
+//         else if (returning)
+//         {
+//             StartCoroutine(PauseThenContinue(returnDelay));
+//         }
+
+        
+
+//     }
+
+//     IEnumerator PauseThenContinue(float _waitTime)
+//     {
+//             yield return new WaitForSeconds(_waitTime);
+//         //gameObject.GetComponent<AudioSource>().Play();
+
+//         if (!returning)
+//         {
+//             playerTrigger = true;
+//             Debug.Log("playerTrigger set True");
+//         }
+//         else
+//         {
+//             returnStarted = true;
+//             Debug.Log("returnStarted set True");
+//         }
+            
+
+            
+//     }
+
+
+
+// }
+
 using UnityEngine;
 using System.Collections;
-using NUnit.Framework;
-using UnityEngine.UIElements;
 
 public class Elevator : MonoBehaviour
 {
     public Transform targetTransform;
     private Vector3 originalPosition;
-    public bool playerTrigger = false;
-    public bool returning = false;
-    public bool returnStarted = false;
-    public float speed = 0.3f;
-    private float velocity;
-    public float playerSenseDelay = 2.0f;
-    public float returnDelay = 3.0f;
+    private bool playerTrigger = false;
+    private bool returning = false;
+    private Vector3 velocity = Vector3.zero;
+    public float smoothTime = 0.3f;
+    public float waitTime = 2.0f;
+    public float returnDelay = 3.0f;  // Time to wait at the target before returning
 
     public void Start()
     {
         originalPosition = transform.position;
+        // Don't modify targetTransform position - it should be set in the Unity Editor
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-       
-            if (collision.gameObject.tag == "Player" && !playerTrigger && !returning)
-            {
-                StartCoroutine(PauseThenContinue(playerSenseDelay));
-            }
+        if (collision.tag == "Player")
+        {
+            StartCoroutine(PauseThenContinue());
+        }
     }
 
     public void FixedUpdate()
     {
-        if (playerTrigger)
+        if (playerTrigger || returning)
         {
+            Vector3 target = playerTrigger ? targetTransform.position : originalPosition;
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
 
-            
-            if (targetTransform.position != transform.position)
+            if (Vector3.Distance(transform.position, target) < 0.01f)
             {
-
-                if (targetTransform.position.y < transform.position.y)
-                    velocity = -speed;
-                else
-                    velocity = speed;
-
-                transform.position = new Vector3(transform.position.x, transform.position.y + velocity, transform.position.z);
-
-                if ((transform.position.y <= targetTransform.position.y && velocity < 0) || (transform.position.y >= targetTransform.position.y && velocity > 0))
+                if (playerTrigger)
                 {
-
                     playerTrigger = false;
-                    returning = true;
-                    Debug.Log("returning");
-                    
-                    Debug.Log("arrived at target: " + targetTransform.position.y);
+                    StartCoroutine(ReturnAfterDelay());
                 }
-            }
-
-        }
-
-        else if (returning && returnStarted)
-        {
-            
-            if (originalPosition != transform.position)
-            {
-
-                if (originalPosition.y < transform.position.y)
-                    velocity = -speed;
                 else
-                    velocity = speed;
-
-                transform.position = new Vector3(transform.position.x, transform.position.y + velocity, transform.position.z);
-
-                if ((transform.position.y <= originalPosition.y && velocity < 0) || (transform.position.y >= originalPosition.y && velocity > 0))
                 {
-                    
                     returning = false;
-                    returnStarted = false;
-                    
-                    Debug.Log("arrived back home: " + originalPosition.y);
                 }
             }
         }
-
-        else if (returning)
-        {
-            StartCoroutine(PauseThenContinue(returnDelay));
-        }
-
-        
-
     }
 
-    IEnumerator PauseThenContinue(float _waitTime)
+    IEnumerator PauseThenContinue()
     {
-            yield return new WaitForSeconds(_waitTime);
-        //gameObject.GetComponent<AudioSource>().Play();
-
-        if (!returning)
-        {
-            playerTrigger = true;
-            Debug.Log("playerTrigger set True");
-        }
-        else
-        {
-            returnStarted = true;
-            Debug.Log("returnStarted set True");
-        }
-            
-
-            
+        yield return new WaitForSeconds(waitTime); // Wait for 2 seconds
+        playerTrigger = true;
+        Debug.Log("playerTrigger set True");
     }
 
-
-
+    IEnumerator ReturnAfterDelay()
+    {
+        yield return new WaitForSeconds(returnDelay);
+        returning = true;
+        Debug.Log("Returning to original position");
+    }
 }
 
