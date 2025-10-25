d5d5cc582bc726dd845e1166d824cc0127b7800e
Schrod <schrosam@gmail.com>
2025-10-25 08:50:59 -0500

    useless includes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
-using Microsoft.Unity.VisualStudio.Editor;
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement Settings")]
