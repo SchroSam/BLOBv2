ee405e0425c6f870a979cf285d7560934cfb9646
Schrod <schrosam@gmail.com>
2025-10-25 14:00:49 -0500

    Fix 0.3.2

--- Assets/sceneTransitionAnim.cs
+++ Assets/sceneTransitionAnim.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class nextFadeHelper : StateMachineBehaviour
+public class sceneTransitionAnim : StateMachineBehaviour
 {
-    public float delay = 0f;
-    public bool disableAfter = true;
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    public string nextScene = "EndingSlides";
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       SceneManager.LoadScene(nextScene);
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -17,14 +17,10 @@ public class nextFadeHelper : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        if (animator.GetComponent<AnimQueue>() != null)
-            animator.GetComponent<AnimQueue>().GotDone(delay);
-        
-        if(disableAfter)
-            animator.enabled = false;
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
